4a303d43158bdfe1062404a20b2d544187473697
Samuel Belo <samuel.belo@al.infnet.edu.br>
2019-07-25 09:17:53 -0300

    adicionando ex7

--- repetição/exercicio7.py
+++ repetição/exercicio7.py
@@ -3,5 +3,11 @@ Faça um programa que leia 5
 números e informe o maior número.
 """
 
-for i in range(1,6):
-    return
+numeros = []
+num = int(input('quantos numeros? '))
+
+for i in range(num):
+    numb = int(input('digite um numero: '))
+    numeros.append(numb)
+    
+print('O maior número é: ', max(numeros)) 
