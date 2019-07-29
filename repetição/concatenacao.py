c530d83febf6e0d33a82e23e0e1c50073bcd6b87
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-07-29 08:34:26 -0300

    sequence types

---
+++ repetição/concatenacao.py
@@ -0,0 +1,8 @@
+lista = [0,2,3]
+lista2 = [6,7,9]
+
+print(lista + lista2)
+
+
+
+print((22,33) + (55,66))
---
+++ repetição/slice.py
@@ -0,0 +1,6 @@
+lista = [16,32,64,128,256,512]
+
+print(lista[1:3])
+
+lista_lista = range(0,51)
+print(list(lista_lista[1:51:2]))
