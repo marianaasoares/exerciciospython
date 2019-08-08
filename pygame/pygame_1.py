48934af5617570fb853de0c9b48e441bd5d52c84
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-08-08 09:11:32 -0300

    Mudanças no Pygame

--- pygame/pygame_1.py
+++ pygame/pygame_1.py
@@ -13,6 +13,9 @@ terminou = False
 #cria relógio
 clock = pygame.time.Clock()
 
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+
 #classe Quadradinho
 class Quadradinho():
     def __init__(self):
@@ -26,19 +29,61 @@ class Quadradinho():
     def desenha(self, tela):
         pygame.draw.rect(tela, self.cor, self.area)
          
+def mostra_tempo(tempo):
+    font = pygame.font.Font(None, 24)
+    text = font.render("Tempo: " + str(tempo) + "s", 1, branco)
+    textpos = text.get_rect(centerx=tela.get_width()/2)
+    tela.blit(text, textpos)
+         
 lista = []
 for i in range(0, quadrados_iniciais):
     q = Quadradinho()
     q.desenha(tela)
     lista.append(q)
 
+#variavel para contar quantas esperas de 50Hz ou 0,02s
+    conta_clocks = 0
+
+# variável para contar quantos segundos se passaram
+conta_segundos = 0
+mostra_tempo(conta_segundos)
 
 while not terminou:
     #checa os eventos do mouse
     for event in pygame.event.get():
+        #Clicando...
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # obtém as coordenadas do mouse na tela
+            pos = pygame.mouse.get_pos()
+            # checa se clicou em algum quadradinho
+            for q in lista:
+                if q.area.collidepoint(pos):
+                    lista.remove(q)
+                    pontos = pontos + 1
+                    
         if event.type == pygame.QUIT:
             terminou = True
             
+    conta_clocks = conta_clocks + 1
+    
+    # A casa 50 conta_clocks, temos 1s (0,02s x 50 = 1s)
+    if conta_clocks == 50:
+        conta_segundos = conta_segundos + 1
+        conta_clocks = 0
+    # cria um novo quadradinho a cada segundo
+    q = Quadradinho()
+    lista.append(q)
+    
+    #limpar tela e atualizar texto
+    tela.fill(preto)
+    
+    #tela foi apagada, desenha os quadrados novamente
+    for q in lista:
+        q.desenha(tela)
+    
+    #mostra tempo atualizado
+    mostra_tempo(conta_segundos)
+    
     #atualiza o desenho na tela
     pygame.display.update()
     
