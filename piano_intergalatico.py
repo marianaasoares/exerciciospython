aee1e96e0004b0e4f549cabbf6506792f3e60f01
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-09-18 08:37:28 -0300

    Adicionando a tupla de acordes

--- piano_intergalatico.py
+++ piano_intergalatico.py
@@ -1,10 +1,40 @@
-piano_inicial = [1,1,1,1,1]
-acordes = ((1,2), (0,4), (0,2))
+from collections import Counter
+piano_inicial = [1]*15
+
+def achar_frequencia(lista_separada):
+    contador_de_freq = Counter(lista_separada)
+    print(contador_de_freq)
+    maior_freq = max(contador_de_freq.values())
+    for elemento in contador_de_freq.items():
+        if elemento[1] == maior_freq:
+            print(elemento)
+            f = elemento[0]
+    return f
 
 def tocar(piano, acorde):
-    frequencia = 1
-    for elemento in range(acorde[0],acorde[1]+1):
-        piano[elemento] = (piano[elemento] +frequencia)%9
-    return piano       
+    lista_separada = piano[acorde[0]:acorde[1]+1]
+    f = achar_frequencia(lista_separada)
+    print("f = ", f)
+    for elemento in range(acorde[0], acorde[1]+1):
+        piano[elemento] = (piano[elemento] + f) % 9
+    print("novo piano = ", piano)
+    print("___________________________")
+    return piano
+acordes = ((10, 12),
+            (4, 5),
+            (1, 14),
+            (6, 10),
+            (9, 11),
+            (11, 12),
+            (9, 13),
+            (8, 9),
+            (5, 7),
+            (11, 13),
+            (8, 10),
+            (11, 12),
+            (11, 13),
+            (8, 14),
+            (3, 9),)
+for acorde in acordes:
+    print(tocar(piano_inicial, acorde))
 
-print(tocar(piano_inicial, acordes[0]))
